3f89d6df37d05b67c8c462879ae46654c08ff05e
Aaron Liang <yichenaaron@gmail.com>
2024-07-20 22:19:37 -0400

    input for setupGame
    pure input for setupGame

--- game.cc
+++ game.cc
@@ -1,9 +1,32 @@
+#include <iostream>
 #include "game.h"
 #include "Players/player.h"
 
 Game::Game(Player *player1, Player *player2) player1{player1}, player2{player2}, currPlayer{player1} {}
 
-void Game::setupGame() {}
+void Game::setupGame() {
+    string op;
+    while (cin >> op) {
+        if (op == "done") break;
+        else if (op == "+") { // add piece to square
+            char p;
+            string sq;
+            cin >> p >> sq;
+            // parse input into rank and row
+            char rank = sq[0];
+            int row = (int) sq[1] - '0'
+        } else if (op == "-") { // delete piece on square
+            string sq;
+            cin >> sq;
+            // parse input into rank and row
+            char rank = sq[0];
+            int row = (int) sq[1] - '0'
+        } else { // set player colour to go first
+            string colour;
+            cin >> colour;
+        }
+    }
+}
 
 void Game::playGame() {}
 
