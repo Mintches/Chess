3e4be3a98f34b3317194f65efd77022078515c9e
Mintches <70064747+Mintches@users.noreply.github.com>
2024-07-20 22:56:08 -0400

    canadian spelling

--- color.h
+++
@@ -1,8 +0,0 @@
-#ifndef COLOR_H
-#define COLOR_H
-
-enum class Color {
-    WHITE, BLACK, BLUE, LIGHT_BLUE
-};
-
-#endif
---
+++ colour.h
@@ -0,0 +1,8 @@
+#ifndef COLOUR_H
+#define COLOUR_H
+
+enum class Colour {
+    WHITE, BLACK, BLUE, LIGHT_BLUE
+};
+
+#endif
