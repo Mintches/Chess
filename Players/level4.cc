19e7cfb413278d4452b8850f82b88277c5971e0d
a26liang <a26liang@uwaterloo.ca>
2024-07-22 17:10:28 -0400

    did minimax frame for level4

--- Players/level4.cc
+++ Players/level4.cc
@@ -1,16 +1,65 @@
 #include <string>
+#include <algorithm>
 #include "computer.h"
 #include "level4.h"
 using namespace std;
 
 const int INF = 9999999;
 
+// minimax heuristic = sums of the values of all pieces for each player
+// maximizing player = white
+// minimizing player = black
+
 int minimax(Board board, int depth, int alpha, int beta, bool mxPlayer) {
+    // minimax has reached a leaf node
     if (verifyCheckmate(Colour::WHITE) || verifyCheckmate(Colour::BLACK) || 
     verifyStalemate(Colour::WHITE) || verifyStalemate(Colour::BLACK) || verifyDraw() || depth == 0) return board.eval();
     
+    if (mxPlayer) { // maximizing player
+        int mxEval = -INF;
+        vector<Move> allMoves = board.possibleMoves(Colour::WHITE);
+        for (auto m: allMoves) {
+            board.makeMove(m); // test move and continue traversing the search tree
+            int eval = minimax(board, depth - 1, alpha, beta, false); // call minimax with minimizing player on next depth
+            board.undoMove(m); // undo the move to try other moves
+            mxEval = max(mxEval, eval);
+            alpha = max(alpha, eval);
+            if (beta <= alpha) break; // alpha-beta pruning to cut off paths that def won't be useful
+        }
+        return mxEval;
+    } else { // minimizing player
+        int minEval = INF;
+        vector<Move> allMoves = board.possibleMoves(Colour::BLACK);
+        for (auto m: allMoves) {
+            board.makeMove(m);
+            int eval = minimax(board, depth - 1, alpha, beta, true); // call minimax with maximizing player on next depth
+            board.undoMove(m);
+            minEval = min(minEval, eval);
+            beta = min(beta, eval);
+            if (beta <= alpha) break;
+        }
+        return minEval;
+    }
 }
 
-string Level4::getMove(Board board) const {
-    return ""; // TODO: return a Level 4 move
+// to-do:
+// eval: return weighted sum of pieces from one player - same thing from other player
+// makeMove: makes a move on the board
+// undoMove: undos the move
+// maybe minimax should work on a copy of the board each turn instead of the actual board
+
+Move Level4::getMove(Board board, int depth) const {
+    Move best;
+    int mxEval = -INF;
+    vector<Move> allMoves = board.possibleMoves(Colour::WHITE);
+    for (auto m: allMoves) {
+        board.makeMove(m);
+        int eval = minimax(board, depth - 1, -INF, INF, false);
+        board.undoMove(m);
+        if (eval > mxEval) {
+            mxEval = eval;
+            best = m;
+        }
+    }
+    return best;
 }
--- Players/level4.h
+++ Players/level4.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Level4: public Computer {
     int minimax(Board board, int depth, int alpha, int beta, bool mxPlayer);
-    string getMove(Board board) const override;
+    string getMove(Board board, int depth) const override;
 };
 
 #endif
