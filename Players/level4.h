b67479ed724e533bbb111dce4bdb7c25699af5bd
m299yang <m299yang@uwaterloo.ca>
2024-07-22 14:20:24 -0400

    added level4

---
+++ Players/level4.h
@@ -0,0 +1,12 @@
+/*#ifndef LEVEL4_H
+#define LEVEL4_H
+
+#include <string>
+#include "../board.h"
+using namespace std;
+
+class Level4: public Computer {
+    string getMove(Board board) const override;
+};
+
+#endif*/
