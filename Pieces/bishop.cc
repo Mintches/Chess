1e67adec22e88119666ac7a64ff2ca32a1a99693
a26liang <a26liang@uwaterloo.ca>
2024-07-22 00:21:53 -0400

    updated verifyMove for bishop

--- Pieces/bishop.cc
+++ Pieces/bishop.cc
@@ -6,7 +6,14 @@ Bishop::Bishop(int row, int col, int player): Square{row, col, player} {}
 Bishop::~Bishop() {} // do nothing
 
 bool Bishop::verifyMove(Board *board, int torow, int tocol) {
+    // check if the bishop can physically move there
     if (abs(torow - row) != abs(tocol - col)) return false;
+    // check if the bishop is not already on that square
+    if (row == torow && col == tocol) return false;
+    // check if the destination square has one of your own pieces
+    if (returnPlayer(board, row, col) == returnPlayer(board, torow, tocol)) return false;
+
+    // go step by step and check each square on the way
     int shiftrow = 1, shiftcol = 1;
     if (torow < row) shiftrow *= -1;
     if (tocol < col) shiftrow *= -1;
--- Pieces/queen.cc
+++ Pieces/queen.cc
@@ -5,7 +5,12 @@ Queen::Queen(int row, int col, int player) : Square(row, col, player) {}
 Queen::~Queen() {} // do nothing
 
 bool Queen::verifyMove(Board *board, int row, int col) {
-    return true;
+    // check if queen can physical move there
+    if ((row != torow && col != tocol) && (abs(torow - row) != abs(tocol - col))) return false;
+    // check if the queen is not already on that square
+    if (row == torow && col == tocol) return false;
+    // check if destination square has one of your own pieces
+    if (returnPlayer(board, row, col) == returnPlayer(board, torow, tocol)) return false;
 }
 
 void Queen::updatePos(int row, int col) {
