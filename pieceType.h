cc1ff3894226e3702d120912d474394803c1c33f
m299yang <m299yang@uwaterloo.ca>
2024-07-23 02:16:49 -0400

    changed bishop ennum

--- pieceType.h
+++ pieceType.h
@@ -5,7 +5,7 @@ enum class PieceType { // follows minimax point procedure
     EMPTY = 0, 
     PAWN = 10, 
     KNIGHT = 30, 
-    BISHOP = 31, 
+    BISHOP = 32, 
     ROOK = 50, 
     QUEEN = 90, 
     KING = 900
