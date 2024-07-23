7bc30ba305659701844268df3d9a9e8c72ae9d4a
Mintches <70064747+Mintches@users.noreply.github.com>
2024-07-23 15:22:07 -0400

    fixed piece verifyMove logic

--- Pieces/knight.cc
+++ Pieces/knight.cc
@@ -4,15 +4,12 @@ Knight::Knight(int row, int col, Colour player) : Square(row, col, player) {}
 Knight::~Knight() {} // do nothing
 
 bool Knight::verifyMove(Board *board, int torow, int tocol) {
-    if (board->getSquare(row, col)->returnPlayer() != player) {
+    if (board->getSquare(row, col)->returnType() == PieceType::EMPTY || board->getSquare(row, col)->returnPlayer() != player) {
         if ((abs(row - torow) == 2 && abs(col - tocol) == 1)
             || (abs(row - torow) == 1 && abs(col - tocol) == 2)) {
             return true;
         }
-        return false;
-    } else {
-        return false;
-    }
+    } else return false;
 }
 
 void Knight::updatePos(int row, int col) {
