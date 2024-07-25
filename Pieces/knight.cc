250e5703f2f2ed060a49cdb13a79a9c641fb8d2d
a26liang <a26liang@uwaterloo.ca>
2024-07-24 23:20:01 -0400

    verifymove for queen and knight

--- Pieces/knight.cc
+++ Pieces/knight.cc
@@ -5,19 +5,17 @@ Knight::~Knight() {} // do nothing
 
 Move Knight::verifyMove(Board *board, int torow, int tocol) {
     Move m;
-    /*
     if (board->getSquare(row, col)->returnType() == PieceType::EMPTY || board->getSquare(row, col)->returnPlayer() != player) {
         if ((abs(row - torow) == 2 && abs(col - tocol) == 1)
             || (abs(row - torow) == 1 && abs(col - tocol) == 2)) {
+            m.addAdded(new EmptySquare(row, col, Colour::BLUE));
+            m.addAdded(new Knight(torow, tocol, player));
+            m.addDeleted(this);
+            m.addDeleted(board->getSquare(torow, tocol));
             return m;
         }
         return m;
     } else return m;
-    */
-    m.addAdded(new EmptySquare(row, col, Colour::BLUE));
-    m.addAdded(new Knight(torow, tocol, player));
-    m.addDeleted(this);
-    m.addDeleted(board->getSquare(torow, tocol));
 }
 
 void Knight::updatePos(int row, int col) {
