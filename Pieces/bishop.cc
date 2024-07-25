99b50a5f3da021eccfa36e0a6e70cc18f7117835
m299yang <m299yang@uwaterloo.ca>
2024-07-24 23:42:37 -0400

    yuh

--- Pieces/bishop.cc
+++ Pieces/bishop.cc
@@ -12,27 +12,26 @@ Bishop::~Bishop() {} // do nothing
 Move Bishop::verifyMove(Board *board, int torow, int tocol) {
     Move m;
     if (abs(torow - row) == abs(tocol - col) && row != torow) { // bishop move limit and that it'll actually moved
-
+        if (board->getSquare(torow, tocol)->returnType() != PieceType::EMPTY 
+        || board->getSquare(torow, tocol)->returnPlayer() != player) { // destination is capture or empty square
+            // go step by step and check each square on the way
+            /*int shiftrow = 1, shiftcol = 1;
+            if (torow < row) shiftrow *= -1;
+            if (tocol < col) shiftrow *= -1;
+            int currow = row, curcol = col;
+            currow += shiftrow;
+            curcol += shiftcol;
+            while (currow < torow) {
+                if (board->getSquare(currow, curcol)->returnType() != PieceType::EMPTY) return m;
+                currow += shiftrow;
+                curcol += shiftcol;
+            }*/
+            m.addAdded(new EmptySquare(row, col, Colour::BLUE));
+            m.addAdded(new Bishop(torow, tocol, player));
+            m.addDeleted(this);
+            m.addDeleted(board->getSquare(torow, tocol));
+        }
     }
-    // check if the destination square has one of your own pieces
-    if (board->getSquare(row, col)->returnPlayer() == board->getSquare(torow, tocol)->returnPlayer()) return m;
-
-    // go step by step and check each square on the way
-    int shiftrow = 1, shiftcol = 1;
-    if (torow < row) shiftrow *= -1;
-    if (tocol < col) shiftrow *= -1;
-    int currow = row, curcol = col;
-    currow += shiftrow;
-    curcol += shiftcol;
-    while (currow != torow) {
-        if (board->getSquare(currow, curcol)->returnType() != PieceType::EMPTY) return m;
-        currow += shiftrow;
-        curcol += shiftcol;
-    }
-    m.addAdded(new EmptySquare(row, col, Colour::BLUE));
-    m.addAdded(new Bishop(torow, tocol, player));
-    m.addDeleted(this);
-    m.addDeleted(board->getSquare(torow, tocol));
     return m;
 }
 
