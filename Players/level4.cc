a9e8661b6f363e734577536471d8a157f720e821
a26liang <a26liang@uwaterloo.ca>
2024-07-25 02:18:30 -0400

    updated minimax

--- Players/level4.cc
+++ Players/level4.cc
@@ -4,38 +4,38 @@
 #include "level4.h"
 using namespace std;
 
+// left to fix: minimax should work on a copy of the board each turn instead of the actual board, otherwise not const
+
 const int INF = 9999999;
 
 // minimax heuristic = sums of the values of all pieces for each player
 // maximizing player = white
 // minimizing player = black
 // eval: return weighted sum of pieces from white (mx player) - same thing from black (min player)
-// maybe minimax should work on a copy of the board each turn instead of the actual board
 
-int minimax(Board board, int depth, int alpha, int beta, bool mxPlayer) {
+int minimax(Board *board, int depth, int alpha, int beta, bool mxPlayer) {
     // minimax has reached a leaf node
-    if (verifyCheckmate(Colour::WHITE) || verifyCheckmate(Colour::BLACK) || 
-    verifyStalemate(Colour::WHITE) || verifyStalemate(Colour::BLACK) || verifyDraw() || depth == 0) return board.evaluate();
+    if (board->verifyCheckmate(Colour::WHITE) || board->verifyCheckmate(Colour::BLACK) || 
+    board->verifyStalemate(Colour::WHITE) || board->begin(Colour::BLACK) 
+    || board->verifyDraw() || depth == 0) return board->evaluate();
     
-    if (mxPlayer) { // maximizing player
+    if (mxPlayer) { // maximizing player (white)
         int mxEval = -INF;
-        vector<Move> allMoves = board.possibleMoves(Colour::WHITE);
-        for (auto m: allMoves) {
-            board.makeMove(m); // test move and continue traversing the search tree
+        for (auto m: board->possibleMoves(Colour::WHITE)) {
+            board->movePiece(m); // test move and continue traversing the search tree
             int eval = minimax(board, depth - 1, alpha, beta, false); // call minimax with minimizing player on next depth
-            board.undoMove(m); // undo the move to try other moves
+            board->undoMove(); // undo the move to try other moves
             mxEval = max(mxEval, eval);
             alpha = max(alpha, eval);
             if (beta <= alpha) break; // alpha-beta pruning to cut off paths that def won't be useful
         }
         return mxEval;
-    } else { // minimizing player
+    } else { // minimizing player (black)
         int minEval = INF;
-        vector<Move> allMoves = board.possibleMoves(Colour::BLACK);
-        for (auto m: allMoves) {
-            board.makeMove(m); // change to board.movePiece
+        for (auto m: board->possibleMoves(Colour::BLACK)) {
+            board->movePiece(m); 
             int eval = minimax(board, depth - 1, alpha, beta, true); // call minimax with maximizing player on next depth
-            board.undoMove(m);
+            board->undoMove();
             minEval = min(minEval, eval);
             beta = min(beta, eval);
             if (beta <= alpha) break;
@@ -44,17 +44,29 @@ int minimax(Board board, int depth, int alpha, int beta, bool mxPlayer) {
     }
 }
 
-Move Level4::getMove(Board board, int depth) const {
+Move Level4::getMove(Board *board, Colour player) const {
     Move best;
-    int mxEval = -INF;
-    vector<Move> allMoves = board.possibleMoves(Colour::WHITE);
-    for (auto m: allMoves) {
-        board.makeMove(m); // change to  board.movePiece
-        int eval = minimax(board, depth - 1, -INF, INF, false);
-        board.undoMove(m);
-        if (eval > mxEval) {
-            mxEval = eval;
-            best = m;
+    if (player == Colour::WHITE) {
+        int mxEval = -INF;
+        for (auto m: board->possibleMoves(Colour::WHITE)) {
+            board->movePiece(m);
+            int eval = minimax(board, 1, -INF, INF, false);
+            board->undoMove();
+            if (eval > mxEval) {
+                mxEval = eval;
+                best = m;
+            }
+        }
+    } else {
+        int minEval = INF;
+        for (auto m: board->possibleMoves(Colour::BLACK)) {
+            board->movePiece(m);
+            int eval = minimax(boardCopy, 1, -INF, INF, true);
+            board->undoMove();
+            if (eval < minEval) {
+                minEval = eval;
+                best = m;
+            }
         }
     }
     return best;
