7bcb9c6b2cbf13763941a40cbb0f4ccbe1da01a2
m299yang <m299yang@uwaterloo.ca>
2024-07-24 23:53:12 -0400

    bishop/input debugging

--- Pieces/bishop.cc
+++ Pieces/bishop.cc
@@ -1,4 +1,5 @@
 #include <utility>
+#include <iostream>
 
 #include "bishop.h"
 #include "emptysquare.h"
@@ -11,21 +12,23 @@ Bishop::~Bishop() {} // do nothing
 
 Move Bishop::verifyMove(Board *board, int torow, int tocol) {
     Move m;
+    std::cout << torow << ":" << tocol;
     if (abs(torow - row) == abs(tocol - col) && row != torow) { // bishop move limit and that it'll actually moved
         if (board->getSquare(torow, tocol)->returnType() != PieceType::EMPTY 
         || board->getSquare(torow, tocol)->returnPlayer() != player) { // destination is capture or empty square
             // go step by step and check each square on the way
-            /*int shiftrow = 1, shiftcol = 1;
+            int shiftrow = 1, shiftcol = 1;
             if (torow < row) shiftrow *= -1;
             if (tocol < col) shiftrow *= -1;
             int currow = row, curcol = col;
             currow += shiftrow;
             curcol += shiftcol;
-            while (currow < torow) {
+            while (currow != torow) {
+                std::cout << currow << ":" << torow << ":" << curcol <<":" << tocol;
                 if (board->getSquare(currow, curcol)->returnType() != PieceType::EMPTY) return m;
                 currow += shiftrow;
                 curcol += shiftcol;
-            }*/
+            }
             m.addAdded(new EmptySquare(row, col, Colour::BLUE));
             m.addAdded(new Bishop(torow, tocol, player));
             m.addDeleted(this);
