2e403461fed2f71b58e40bc753e5c4ecb92c4cb0
m299yang <m299yang@uwaterloo.ca>
2024-07-24 23:11:20 -0400

    king logic

--- Pieces/king.cc
+++ Pieces/king.cc
@@ -8,14 +8,21 @@ King::~King() {} // do nothing
 Move King::verifyMove(Board *board, int torow, int tocol) {
     Move m;
     if (abs(torow - row) + abs(tocol - col) == 1 && (abs(torow - row) == 1 || abs(tocol - col) == 1)) { // king move limits
-        if (board->getSquare(torow, tocol)->returnPlayer() != player) { // check if capture
-            if (board->getSquare(torow, tocol)->returnType() != PieceType::KING) { // can't capture other king
-                return m;
+        // ~two kings, chillin in a hot tub, 5 ft apart cuz~
+        for (int i = -1; i < 2; ++i) {
+            for (int j = -1; j < 2; ++j) {
+                if (board->getSquare(torow + i, tocol + j)->returnType() == PieceType::KING && i != 0 && j != 0) {
+                    return m;
+                }
             }
         }
+        m.addAdded(new EmptySquare(row, col, Colour::BLUE));
+        m.addAdded(new King(torow, tocol, player));
+        m.addDeleted(this);
+        m.addDeleted(board->getSquare(torow, tocol));
     }
     return m;
-} // maybe check that it's not right beside another king? althought verifyCheck() should deal with that case
+}
 
 void King::updatePos(int row, int col) {
 
