5fa12d4774bf001711dccc59f78a1e6ffcd73c41
m299yang <m299yang@uwaterloo.ca>
2024-07-25 00:07:38 -0400

    added board border

--- Observers/textObserver.cc
+++ Observers/textObserver.cc
@@ -8,13 +8,14 @@ void TextObserver::notify() {
     int vertBorder = 0;
     for (int i = 0; i < 8; ++i) {
         ++vertBorder;
-        cout << vertBorder;
+        cout << vertBorder << " ";
         for (int j = 0; j < 8; ++j) {
             cout << sub->getState(i, j);
         }
         cout << endl;
     }
     // add horizontal border of letters
+    cout << "  ";
     for (int i = 'a'; i <= 'h'; ++i) {
         char horBorder = i;
         cout << horBorder;
